b867c4bc7d950e493a1d0da533ededf90116b4ff
merna sameh <112027112+merna216@users.noreply.github.com>
2026-02-17 15:58:16 +0200

    Add health check and user creation endpoints

--- services/api-gateway/main.py
+++ services/api-gateway/main.py
@@ -1,6 +1,31 @@
 from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
+from pydantic import BaseModel
 
+app = FastAPI()
+
+@app.get("/")
 async def root():
     return {"message": "Mini Snap API is running 🚀"}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+class User(BaseModel):
+    name: str
+    age: int
+
+
+@app.post("/users")
+async def create_user(user: User):
+    return {
+        "message": "User created successfully",
+        "user": user
+    }
